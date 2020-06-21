ab4527997cd009b2865c65588ef983090851adb1
nevtay <nevintay@gmail.com>
2020-06-21 20:25:33 +0800

    Place project title inside <h1> instead of the <nav> element itself

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <nav>Pomodoro TImer</nav>
+      <nav>
+        <h1>Pomodoro TImer</h1>
+      </nav>
     </div>
   );
 }
