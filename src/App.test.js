f3fed5286405d69489f88aba56043cbae11e783a
nevtay <nevintay@gmail.com>
2020-06-21 16:30:23 +0800

    Replace default test with a test for project title's presence

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Project title is in the document", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const projectTitle = getByText(/Pomodoro Timer/i);
+  expect(projectTitle).toBeInTheDocument();
 });
