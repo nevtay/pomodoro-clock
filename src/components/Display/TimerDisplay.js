ec9552e13f019649d4eb2d1c03014e47e401ca7c
nevtay <nevintay@gmail.com>
2020-06-21 22:10:29 +0800

    Add start, pause, and reset buttons

--- src/components/Display/TimerDisplay.js
+++ src/components/Display/TimerDisplay.js
@@ -1,5 +1,21 @@
 import React from "react";
+import ButtonTemplate from "../Buttons/ButtonTemplate";
 
 export default function TimerDisplay() {
-  return <h1>25:00</h1>;
+  return (
+    <div>
+      <div>
+        <h1>25:00</h1>
+      </div>
+      <div>
+        <ButtonTemplate text="Start" />
+      </div>
+      <div>
+        <ButtonTemplate text="Pause" />
+      </div>
+      <div>
+        <ButtonTemplate text="Reset" />
+      </div>
+    </div>
+  );
 }
