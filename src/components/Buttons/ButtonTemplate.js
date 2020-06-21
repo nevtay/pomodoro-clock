b4424a24a66fde37e5419171536896df6e1ac31e
nevtay <nevintay@gmail.com>
2020-06-21 21:05:06 +0800

    Initial commit

---
+++ src/components/Buttons/ButtonTemplate.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ButtonTemplate(prop) {
+  return <button>{prop.prop}</button>;
+}
---
+++ src/components/Buttons/ButtonTemplate.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ButtonTemplate from "./ButtonTemplate";
+
+test("Button Text should accept a prop", () => {
+  const testText = "test";
+  const { getByText } = render(<ButtonTemplate prop={testText} />);
+  const buttonText = getByText(/TEST/i);
+  expect(buttonText.innerHTML).toEqual("test");
+});
