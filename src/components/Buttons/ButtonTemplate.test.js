5a7ceb94c295ad9dcb4d1076611d3f6cec4e8996
nevtay <nevintay@gmail.com>
2020-06-23 16:10:55 +0800

    Change expected test result from hard-coded string to the variable where string is stored

--- src/components/Buttons/ButtonTemplate.test.js
+++ src/components/Buttons/ButtonTemplate.test.js
@@ -6,5 +6,5 @@ test("Button Text should accept a prop", () => {
   const testText = "test";
   const { getByText } = render(<ButtonTemplate text={testText} />);
   const buttonText = getByText(/TEST/i);
-  expect(buttonText.innerHTML).toEqual("test");
+  expect(buttonText.innerHTML).toEqual(testText);
 });
