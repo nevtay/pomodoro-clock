39564598feb62d792724d3ab80e7249f79f1e4fe
nevtay <nevintay@gmail.com>
2020-06-24 01:16:09 +0800

    Remove unused React hooks

--- src/components/Display/TimerDisplay.js
+++ src/components/Display/TimerDisplay.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import ButtonTemplate from "../Buttons/ButtonTemplate";
 
 export default function TimerDisplay() {
