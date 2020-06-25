d963ca0e2d0048ba35d7752ff3a6b4b852d32d7a
nevtay <nevintay@gmail.com>
2020-06-25 19:17:23 +0800

    Update test name to better reflect intent

--- src/components/Buttons/ButtonTemplate.test.js
+++ src/components/Buttons/ButtonTemplate.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ButtonTemplate from "./ButtonTemplate";
 
-test("Button Text should accept a prop", () => {
+test("Button component should accept a text prop", () => {
   const testText = "test";
   const { getByText } = render(<ButtonTemplate text={testText} />);
   const buttonText = getByText(testText);
