8b0c76434fa1b391edc5eeb533f75ef6ac8fbc42
nevtay <nevintay@gmail.com>
2020-06-25 18:20:03 +0800

    Add test to check for presence of buttons

--- src/components/Display/TimerDisplay.test.js
+++ src/components/Display/TimerDisplay.test.js
@@ -8,8 +8,29 @@ test("TimerDisplay shows 25:00 by default", () => {
   expect(defaultStartTime).toBeInTheDocument();
 });
 
-test("TimerDisplay has correct number of buttons", () => {
+test("TimerDisplay has at least buttons", () => {
   const { getAllByLabelText } = render(<TimerDisplay />);
   const buttons = getAllByLabelText(/button/i);
   expect(buttons.length).toEqual(3);
+});
+
+test("TimerDisplay has a start button", () => {
+  const { getByText } = render(<TimerDisplay />);
+  const startButton = getByText(/Start/i);
+  expect(startButton.tagName).toEqual("BUTTON");
+  expect(startButton.innerHTML).toEqual("Start");
+});
+
+test("TimerDisplay has a pause button", () => {
+  const { getByText } = render(<TimerDisplay />);
+  const pauseButton = getByText(/Pause/i);
+  expect(pauseButton.tagName).toEqual("BUTTON");
+  expect(pauseButton.innerHTML).toEqual("Pause");
+});
+
+test("TimerDisplay has a reset button", () => {
+  const { getByText } = render(<TimerDisplay />);
+  const resetButton = getByText(/Reset/i);
+  expect(resetButton.tagName).toEqual("BUTTON");
+  expect(resetButton.innerHTML).toEqual("Reset");
 });
