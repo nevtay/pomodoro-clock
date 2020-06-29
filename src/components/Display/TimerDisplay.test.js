f7c85299edd1edff5376602d0acd98d196e90d1a
nevtay <nevintay@gmail.com>
2020-06-30 00:19:30 +0800

    Add test to check that timer counts down one second at a time and displays the correct time
    Error message implying a memory leak to be investigated

--- src/components/Display/TimerDisplay.test.js
+++ src/components/Display/TimerDisplay.test.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { render, fireEvent, waitFor } from "@testing-library/react";
+import { render, fireEvent, waitFor, act } from "@testing-library/react";
 import TimerDisplay from "./TimerDisplay";
+
+let cleanupFunc;
 
 test("TimerDisplay shows 25:00 by default", () => {
   const { getByText } = render(<TimerDisplay />);
@@ -35,6 +37,14 @@ test("TimerDisplay has a reset button", () => {
   expect(resetButton.innerHTML).toEqual("Reset");
 });
 
+test("TimerDisplay time should display 25:00 (25 minutes) by default", async () => {
+  jest.useFakeTimers();
+  const { getByText, getByLabelText } = render(<TimerDisplay />);
+  const startButton = getByText(/Start/i);
+  const timeLeft = getByLabelText(/timer-display/i);
+  expect(timeLeft.innerHTML).toEqual("25:00");
+});
+
 test("TimerDisplay start button should trigger setInterval()", () => {
   jest.useFakeTimers();
   const { getByText } = render(<TimerDisplay />);
@@ -44,10 +54,16 @@ test("TimerDisplay start button should trigger setInterval()", () => {
   expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1000);
 });
 
-test("TimerDisplay time should display 25:00 (25 minutes) by default", async () => {
+test("TimerDisplay time should count down one second at a time", () => {
   jest.useFakeTimers();
   const { getByText, getByLabelText } = render(<TimerDisplay />);
   const startButton = getByText(/Start/i);
   const timeLeft = getByLabelText(/timer-display/i);
-  expect(timeLeft.innerHTML).toEqual("25:00");
+  act(() => {
+    fireEvent.click(startButton);
+    jest.advanceTimersByTime(1000);
+    expect(timeLeft.innerHTML).toEqual("24:59");
+    jest.advanceTimersByTime(2000);
+    expect(timeLeft.innerHTML).toEqual("24:57");
+  });
 });
