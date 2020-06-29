f2fea328632ee6fc0419816214f1892ca1c0a9d6
nevtay <nevintay@gmail.com>
2020-06-30 00:55:46 +0800

    Remove 'async' keyword as test doesn't need async functionality

--- src/components/Display/TimerDisplay.test.js
+++ src/components/Display/TimerDisplay.test.js
@@ -35,7 +35,7 @@ test("TimerDisplay has a reset button", () => {
   expect(resetButton.innerHTML).toEqual("Reset");
 });
 
-test("TimerDisplay time should display 25:00 (25 minutes) by default", async () => {
+test("TimerDisplay time should display 25:00 (25 minutes) by default", () => {
   const { getByText, getByLabelText } = render(<TimerDisplay />);
   const startButton = getByText(/Start/i);
   const timeLeft = getByLabelText(/timer-display/i);
