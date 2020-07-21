ade270df12f30be7d0879b4140cb523266175344
nevtay <nevintay@gmail.com>
2020-07-21 22:07:14 +0800

    Add css classes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="background-gradient background-gradient-filter">
       <Navbar />
       <TimerDisplay />
     </div>
